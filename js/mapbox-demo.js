7fc5fa98a62cf9d5111ce386c20609d250a6cd02
Daniel Estrada <daniel.nicholas.estrada@gmail.com>
2023-11-27 14:50:10 -0600

    mapbox lecture

--- js/mapbox-demo.js
+++ js/mapbox-demo.js
@@ -1,26 +1,74 @@
 import {keys} from "./keys.js";
 
+const getCoordinates = async (searchText) => {
+     searchText = encodeURIComponent(searchText);
+     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?access_token=${keys.mapbox}`;
+     const options = {
+          method: "GET",
+          headers: {
+               "Content-Type": "application/json",
+          },
+     };
+     const response = await fetch(url, options);
+     const data = await response.json();
+     return data.features[0].center;
+}
+
+const getAddress = async (lng, lat) => {
+     if(Array.isArray(lng)) {
+          lat = lng[1];
+          lng = lng[0];
+     }
+     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${keys.mapbox}`;
+     const options = {
+          method: "GET",
+          headers: {
+               "Content-Type": "application/json",
+          },
+     };
+     const response = await fetch(url, options);
+     const data = await response.json();
+     return data.features[0].place_name;
+}
+
+const handleDragEnd = async (e, map) => {
+     const lng = e.target._lngLat.lng;
+     const lat = e.target._lngLat.lat;
+     map.flyTo({
+          center: [lng, lat],
+          zoom: 12,
+          speed: 2,
+     });
+     const address = await getAddress(lng, lat);
+     console.log(address);
+}
+
 (async()=>{
+     const coordinates = await getCoordinates("8700 Tesoro Dr Suite 100, San Antonio, TX 78217");
      mapboxgl.accessToken = keys.mapbox;
      const map = new mapboxgl.Map({
           container: 'map', // container ID
           // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-          style: 'mapbox://styles/mapbox/streets-v12', // style URL
-          center: [-98.4916, 29.4252], // starting position [lng, lat]
+          style: 'mapbox://styles/mapbox/navigation-night-v1', // style URL
+          center: coordinates, // starting position [lng, lat]
           zoom: 9 // starting zoom
      });
 
-     const marker = new mapboxgl.Marker()
-     .setLngLat([-98.4916, 29.4260])
-     .addTo(map);
+     map.flyTo({
+          center: coordinates,
+          zoom: 16,
+     });
 
      const popup = new mapboxgl.Popup()
-     .setLngLat([-98.4916, 29.4260])
-     .setHTML("<p>We don't live here anymore</p>")
-     .addTo(map);
+          .setHTML("<p>We don't live here anymore</p>")
 
-     map.flyTo({
-          center: [-98.4916, 29.4252],
-          zoom: 16,
+     const marker = new mapboxgl.Marker({
+          draggable: true,
+     })
+          .setLngLat(coordinates)
+          .addTo(map)
+          .setPopup(popup);
+     marker.on("dragend", e=> {
+          handleDragEnd(e, map);
      });
 })();
