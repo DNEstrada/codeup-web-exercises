15ffb60866b3e2c9d50ed2e3496fbbcc907fed7f
Daniel Estrada <daniel.nicholas.estrada@gmail.com>
2023-11-29 11:12:31 -0600

    draggable

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,6 +1,6 @@
 import {keys} from "./keys.js";
 
-const getForecast = (lat, lng) => {
+const getForecast = (lat=29.4587654, lng=-98.8440411) => {
      if (Array.isArray(lat)) {
           lng = lat[1];
           lat = lat[0];
@@ -12,12 +12,14 @@ const getForecast = (lat, lng) => {
      return fetch(url, options)
           .then(response=>response.json())
           .then((weather)=> {
-               return weather;
+               return renderCards(weather);
           })
           .catch(error=>error);
 };
 
 const renderCards = weather => {
+     let cardBody = document.querySelector('main .col');
+     cardBody.innerHTML = "";
      for(let i = 0; i<5; i++) {
           const weatherCard = document.createElement('article');
           weatherCard.classList.add('weather-card', 'col-2');
@@ -30,51 +32,90 @@ const renderCards = weather => {
           <p>Wind: ${weather.daily[i].wind_speed}</p>
           <p>Pressure: ${weather.daily[i].pressure}</p>
           `;
-          document.querySelector('main .col').appendChild(weatherCard);
+          cardBody.appendChild(weatherCard);
      }
 }
 
-const getMap = (lat=-98.8440411, lng=29.4587654) => {
+const getMap = (lat=29.4587654, lng=-98.8440411) => {
      mapboxgl.accessToken = keys.mapbox;
+
      const map = new mapboxgl.Map({
           container: 'map',
           style: 'mapbox://styles/mapbox/streets-v12',
-          center: [lat, lng],
+          center: [lng, lat],
           zoom: 5
      });
+
      map.addControl(new mapboxgl.NavigationControl());
-     map.addControl(
-               new MapboxGeocoder({
-              accessToken: mapboxgl.accessToken,
-              mapboxgl: mapboxgl
-         })
-     );
+
+     let geocoder = new MapboxGeocoder({
+          accessToken: mapboxgl.accessToken,
+          marker: false,
+          mapboxgl: mapboxgl
+     });
+     map.addControl(geocoder);
+
      const popup = new mapboxgl.Popup();
-     let marker = new mapboxgl.Marker()
-     .setLngLat([lat, lng])
+     let currentMarkers = [];
+     let marker = new mapboxgl.Marker({
+          draggable: true,
+     })
+     .setLngLat([lng, lat])
      .addTo(map)
      map.flyTo({
-          center: [lat, lng],
+          center: [lng, lat],
           zoom: 5,
-          speed: 2,
+          speed: 5,
      });
+
+     marker.on("dragend", e=> {
+          map.flyTo({
+               center: [e.target._lngLat.lng, e.target._lngLat.lat],
+               zoom: 10,
+               speed: 1,
+          });
+          getForecast(e.target._lngLat.lat, e.target._lngLat.lng);
+     });
+
      map.on('click', e=> {
-          marker.remove();
-          marker = new mapboxgl.Marker()
+          clearMarkers(currentMarkers);
+          marker = new mapboxgl.Marker({
+               draggable: true,
+          })
           .setLngLat([e.lngLat.lng, e.lngLat.lat])
           .addTo(map)
           map.flyTo({
                center: [e.lngLat.lng, e.lngLat.lat],
-               zoom: 5,
-               speed: 2,
+               zoom: 10,
+               speed: 1,
+          });
+          getForecast(e.lngLat.lat, e.lngLat.lng);
+     });
+
+     geocoder.on('result', e => {
+          marker.remove();
+          getForecast(e.result.center[1], e.result.center[0]);
+          marker = new mapboxgl.Marker({
+               draggable: true,
+          })
+          .setLngLat([e.result.center[0], e.result.center[1]])
+          .addTo(map)
+          marker.on("dragend", e=> {
+               map.flyTo({
+                    center: [e.target._lngLat.lng, e.target._lngLat.lat],
+                    zoom: 10,
+                    speed: 1,
+               });
+               getForecast(e.target._lngLat.lat, e.target._lngLat.lng);
           });
      });
+};
+
+const clearMarkers = () => {
+     document.querySelector(".mapboxgl-marker").remove();
 }
 
 (()=>{
      getMap();
-     getForecast(0, 0)
-          .then(weather=> {
-               renderCards(weather);
-          });
+     getForecast();
 })();
